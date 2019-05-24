db723183f779bccf1d9c8ef0dfd8497ef3876735
Libertad Chapinal <libertadchapinalcervantes@gmail.com>
2019-05-24 18:27:48 +0200

    Publish github pages

---
+++ docs/precache-manifest.60cdf3389171daad8505eda42b8bab13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f344e8bb3bb051da2d84a6814edd7627",
+    "url": "./index.html"
+  },
+  {
+    "revision": "46fb5a3098d25f300b79",
+    "url": "./static/css/main.a08207e2.chunk.css"
+  },
+  {
+    "revision": "6eae9c25acac3efe3859",
+    "url": "./static/js/2.b75ea3cb.chunk.js"
+  },
+  {
+    "revision": "46fb5a3098d25f300b79",
+    "url": "./static/js/main.e57162fa.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
